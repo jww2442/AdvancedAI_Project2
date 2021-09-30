e568602557005555bb0ce9abee676ff19d247413
BuildTools <unconfigured@null.spigotmc.org>
2021-09-30 11:11:51 -0500

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+#Project 2 for the University of Tulsa'ss  CS-7313 Adv. AI Course
+#Probabilistic Reasoning Over Time
+#Professor: Dr. Sen, Fall 2021
+#Noah Schrick
+
+def main():
+    print("Hello")
+
+if __name__ == '__main__':
+    main()
